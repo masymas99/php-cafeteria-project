9372cd9d6ef76abf41d1335aabf5c8a3ccf6d0a2
masymas99 <masoodadel1999@gmail.com>
2025-01-15 08:46:08 +0200

    fetched all users

---
+++ all user/Products.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- boot strab -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&family=Cairo:wght@200..1000&family=Outfit:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Admin Dashboard</title>
+</head>
+
+<body>
+    <aside>
+        <h1>C A F E T E R I A</h1>
+        <nav>
+            <ul>
+                <li><i  class="fa-solid fa-house" class="navitems"></i><a href="../index.php">Home</a></li>
+                <li><i class="fa-solid fa-basket-shopping"  class="navitems"></i><a href="Products.php" target="_blank">Products</a></li>
+                <li><i class="fa-solid fa-users" class="navitems"></i><a href="#">Users</a></li>
+                <li><i class="fa-solid fa-receipt" class="navitems"></i><a href="#">Orders</a></li>
+                <li><i class="fa-solid fa-money-check" class="navitems"></i><a href="#">Checks</a></li>
+            </ul>
+        </nav>
+
+        <div class="vr1"></div>
+    </aside>
+    <!-- <div class="vr2"></div> -->
+
+    <hr>
+
+    <main>
+        <div class="container">
+            <div class="header">
+                <h1> All Users</h1>
+                <a href="#" onclick="openCart()" class="btn-add-products">
+                    <i class="fa-solid fa-plus"></i>
+                    <h3>Add user</h3>
+                </a>
+            </div>
+
+
+            <div class="Addproduct-modal" id="Addproductmodal">
+                <div class="Addproduct-content">
+                    <h2>Add New user </h2>
+                   <form action="" >
+                   <input class="form-control" type="text" placeholder="user Name" name="name" aria-label="default input example">
+                   <input class="form-control" type="Email" placeholder="Email" name="Email" aria-label="default input example">
+                   <input class="form-control" type="password" placeholder="password" name="password" aria-label="default input example">
+                   <input class="form-control" type="password" placeholder="comfirm password" name="comfirm password" aria-label="default input example">
+                   <input class="form-control" type="text" placeholder="Room Number " name="Room Number" aria-label="default input example">
+                   <input class="form-control" type="file" placeholder="product image" name="product image" aria-label="default input example">
+                   <!-- <div class="Addproduct-actions"> -->
+                    
+                    <button onclick="closeCart()" type="submit" value="Add product">Add User
+                   <!-- </div> -->
+                   </form>
+                   
+                </div>
+            </div>
+
+
+
+            <div class="product">
+
+                <?php
+                require('db.php');
+                $query = "SELECT * FROM users";
+                $statment = $connection->prepare($query);
+                $statment->execute();
+                $users = $statment->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($users as $user): ?>
+                    <div class="product-card" data-product-id="<?php echo $user['UserID']; ?>">
+                        <div class="vr3"></div>
+
+                        <img src="<?php echo $user['ProfileImage']; ?>" alt="<?php echo $user['UserName']; ?>">
+                        <h2><?php echo $user['UserName']; ?></h2>
+                        <h3 class="Roomnumber"> <?php echo "Room Number :  ". $user['RoomNumber']; ?></h3>
+                        <div class="product-actions">
+                            <!-- <div class="add-one" data-action="add"><i class="fa-solid fa-plus"></i></div>
+                            <div class="product-quantity" data-quantity="1">1</div>
+                            <div class="remove-one" data-action="remove"><i class="fa-solid fa-minus"></i></div> -->
+                        </div>
+                        <div class="product-action">
+                            <i class="fa-solid fa-edit"></i>
+                            <i class="fa-solid fa-list"></i>
+                            <i class="fa-solid fa-trash"></i>
+                        </div>
+                        <!-- <a href="#" class="add-to-cart" data-action="add-to-cart">Add to Cart</a> -->
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+
+    </main>
+
+
+
+    <div class="login-info">
+        <div class="login-picture"></div>
+        <h2>Admin</h2>
+    </div>
+
+
+    <script src="script.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
+</body>
+
+</html>
